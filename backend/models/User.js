108df30ebf3f0b3539c1ddc67e7c63ba4a796c5f
25pite02-cell <25pite02@ldc.edu.in>
2026-07-19 13:27:14 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,14 +1,13 @@
 // backend/models/User.js
 const mongoose = require('mongoose');
-
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, enum: ['user', 'admin'], default: 'user' }
+    password: { type: String, required: function () { return !this.googleId; } },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+    googleId: { type: String, default: null }
   },
   { timestamps: true }
 );
-
 module.exports = mongoose.model('User', userSchema);
